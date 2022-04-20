bf8bd797a170bccf49b3ed00f338cc8fcc64223f
AliBaydur <59739580+AliBaydur@github.com>
2022-04-20 22:32:44 +0300

    Add project files.

---
+++ DialogBoxes(OpenDialog_SaveDialog_FontDialog_ColorDialog)/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace DialogBoxes_OpenDialog_SaveDialog_FontDialog_ColorDialog_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_Yukle = new System.Windows.Forms.Button();
+            this.btn_Kaydet = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(14, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(397, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(97, 470);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(225, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btn_Yukle
+            // 
+            this.btn_Yukle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_Yukle.Location = new System.Drawing.Point(97, 503);
+            this.btn_Yukle.Name = "btn_Yukle";
+            this.btn_Yukle.Size = new System.Drawing.Size(225, 33);
+            this.btn_Yukle.TabIndex = 2;
+            this.btn_Yukle.Text = "Yükle";
+            this.btn_Yukle.UseVisualStyleBackColor = true;
+            this.btn_Yukle.Click += new System.EventHandler(this.btn_Yukle_Click);
+            // 
+            // btn_Kaydet
+            // 
+            this.btn_Kaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_Kaydet.Location = new System.Drawing.Point(97, 542);
+            this.btn_Kaydet.Name = "btn_Kaydet";
+            this.btn_Kaydet.Size = new System.Drawing.Size(225, 33);
+            this.btn_Kaydet.TabIndex = 2;
+            this.btn_Kaydet.Text = "Kaydet";
+            this.btn_Kaydet.UseVisualStyleBackColor = true;
+            this.btn_Kaydet.Click += new System.EventHandler(this.btn_Kaydet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.DarkRed;
+            this.ClientSize = new System.Drawing.Size(423, 591);
+            this.Controls.Add(this.btn_Kaydet);
+            this.Controls.Add(this.btn_Yukle);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Resim Yükle ve Kaydet";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_Yukle;
+        private System.Windows.Forms.Button btn_Kaydet;
+    }
+}
+
---
+++ DialogBoxes(OpenDialog_SaveDialog_FontDialog_ColorDialog)/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DialogBoxes_OpenDialog_SaveDialog_FontDialog_ColorDialog_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Yukle_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "PNG Dosyası(*.png) | *.png | JPG Dosyası | *.jpg | Tüm Uzantılar (*.*) | *.*";
+            ofd.FilterIndex = 3;
+
+            DialogResult dr = ofd.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                string DosyaAdi = ofd.SafeFileName;
+                string DosyaYolu = ofd.FileName;
+
+                pictureBox1.ImageLocation = DosyaYolu;
+                textBox1.Text = DosyaAdi;
+            }
+        }
+
+        private void btn_Kaydet_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog svf = new SaveFileDialog();
+            svf.Filter = "PNG Dosyası(*.png) | *.png | JPG Dosyası | *.jpg | Tüm Uzantılar (*.*) | *.*";
+            svf.FilterIndex = 2;
+            svf.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            if (svf.ShowDialog() == DialogResult.OK)
+            {
+                string uzantı = System.IO.Path.GetExtension(svf.FileName);
+                ImageFormat format = ImageFormat.Png;
+
+                switch (uzantı)
+                {
+                    case ".jpg":
+                        format = ImageFormat.Jpeg;
+                        break;
+                    case ".bmp":
+                        format = ImageFormat.Bmp;
+                        break;
+                }
+                pictureBox1.Image.Save(svf.FileName, format);
+            }
+        }
+    }
+}
